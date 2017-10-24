5305b7ab27a3d48fd3e0d7a15d0e2d8809a02267
Kai Dale <Kai.Dale@education.nsw.gov.au>
2017-10-25 10:38:37 +1100

    guessmynumber succesfully completed

---
+++ guessmynumber/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace guessmynumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int rand = rnd.Next(1, 101);
+
+            int guess = -1;
+            int count = 0;
+            while(guess != rand)
+
+            while(guess != rand)
+            {
+                    count++;
+                Console.Write("guess: ");
+                guess = int.Parse(Console.ReadLine());
+                if (guess < rand)
+                {
+                    Console.WriteLine("too low!");
+                }
+                else if (guess > rand)
+                {
+                    Console.WriteLine("too high!");
+                }
+                else
+                {
+                    Console.WriteLine($"you got it, it took {count} tries!");
+                    Console.ReadKey();
+                }
+         
+            }
+        }
+    }
+}
