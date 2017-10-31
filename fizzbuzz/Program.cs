e4ce6dfe7af7f2055582d42d446a6aa4a4817c7f
Kai Dale <Kai.Dale@education.nsw.gov.au>
2017-11-01 09:39:34 +1100

    made fizzbuzz succesfully

---
+++ fizzbuzz/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("fizz buzz");
+                    Console.Beep(1000, 100);
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("fizz");
+                    Console.Beep(2000, 100);
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("buzz");
+                    Console.Beep(3000, 100);
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }      
+        }
+
+    }
+}
