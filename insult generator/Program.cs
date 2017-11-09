c2803b9ead7c0212d97839ff7c83afc3c51a6eab
Kai Dale <Kai.Dale@education.nsw.gov.au>
2017-11-09 12:42:09 +1100

    completed insult generator

--- insult generator/Program.cs
+++ insult generator/Program.cs
@@ -13,38 +13,46 @@ namespace insult_generator
             Random rnd = new Random();
 
             // ask for your name
-            string name = getname()
+            string name = getname();
 
-                // generate a random insult 
+            // generate a random insult 
             Generateinsult(name, rnd);
         }
-        private static string getname()
+        private static string getname()        
         {
-            {
-                Console.WriteLine("welcome to the insult generator. what is your name?");
-                return Console.ReadLine();
-            }
-            private static void Generateinsult(string name, Random rnd)
-            {
-                Console.WriteLine(name + ", you are " + Getadjective() + getcompoundadjective() + getobjective());
-                menu(name, rnd);
-            }
-            private static void menu(string name, Random rnd)...
-            {
-                private static string getadjective()
-            }
+            Console.WriteLine("welcome to the insult generator. what is your name?");
+            return Console.ReadLine();
+        }
+        private static void Generateinsult(string name, Random rnd)
+        {
+            Console.WriteLine(name + ", you are " + getadjective(rnd) + getcompoundadjective(rnd) + getobject(rnd));
+            menu(name, rnd);
+        }
+        private static void menu(string name, Random rnd)
+        {
+            Console.WriteLine("would you like another insult? y/n ");
+            if (Console.ReadLine() == "y")
+
+                Generateinsult(name, rnd);
+        }
+
+        private static string getadjective(Random rnd)
+        {
             string[] adjectives = { "a noisy", "a obnoxious", "a arrogant", "a troll", "a funky", "a loud", "a imbred", "a hairy" };
+            return adjectives[rnd.Next(8)];
+        }
 
-            {
-                private static string getcompoundadjective()
-            }
+        private static string getcompoundadjective(Random rnd)
+        {
             string[] compoundadjectives = { "no-good", "self-loving", "pompus-f**k", "teachers-pet", "uneducated-idiot", "guinea-pig", "cold-hearted", "big-headed" };
-            {
-                private static string getobject()
-            }
-            string[] adjectives = { "monkey", "crabbbbbbbbbb", "chilld", "tree", "cow", "computer", "table", "chair" };
-            {
+            return compoundadjectives[rnd.Next(8)];
+        }
 
-            }
+        private static string getobject(Random rnd)
+        {
+            string[] adjectives = { "monkey", "crabbbbbbbbbb", "chilld", "tree", "cow", "computer", "table", "chair" };
+            return adjectives[rnd.Next(11)];
+        }
+        
     }
 }
